43a76bc6fc60ab7145374aa9480f79cc6572bbd9
Eduardo G <eduardogomes5577@gmail.com>
2025-03-25 16:12:07 -0300

    classificacao das criancas

---
+++ Class c.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main() 
+{
+ int i; 
+  if (i<2)
+  {
+    printf("Bebe");
+  }
+  else{
+    if (i<5)
+    {printf("Crianca");
+    }
+    else 
+    printf("Pre-adolecente");
+  }
+}
