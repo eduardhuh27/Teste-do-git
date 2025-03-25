61f4d77b0c00b9a4dc3a7f0ef454c26cd671386c
Eduardo G <eduardogomes5577@gmail.com>
2025-03-25 15:57:36 -0300

    hello world para selecao de faixa etaria

--- git.c
+++ git.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
-void main(){
-printf("hello world");
+void main()
+{
+ int i;
+  printf("Entre com a usa idede: ");
+  scanf("%d" , &i);
 
-
-
-
+  if (i<12){
+  printf("Crianca");
+  }
+  else  
+  {
+    if  (i<18){
+    printf("Adolecente");
+    }
+    else{
+      printf("Adulto");
+    }
+  }
 }
